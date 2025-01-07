0af48d40d15f246d87073629ef94c3a2f69490f4
agrange7 <alexgrange7@gmail.com>
2025-01-07 19:48:35 -0300

    Fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Directors } from "./components/pages/directors/Directors";
 import { Films } from "./components/pages/films/Films/Films";
 import { Home } from "./components/pages/home/Home";
 import { Lists } from "./components/pages/lists/Lists";
-import { SignUp } from "./components/pages/signUp/SignUp";
+import { SignUp } from "./components/pages/signup/SignUp ";
 import { Root } from "./root/root";
 
 function App() {
